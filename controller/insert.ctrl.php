814d7437461e08134e9d246397390d4bffd5ffab
Ryan Schurton <ryan.schurton@gmail.com>
2016-04-05 23:55:16 -0400

    function to submit post data

--- controller/insert.ctrl.php
+++ controller/insert.ctrl.php
@@ -4,35 +4,23 @@ require('../model/insert.model.php');
 	if($_POST['name']) {
 
 		//Get Post Values when form is submitted 
-		//Read to send off to Insert
-
+		//Convert to output array and use values as index 
 		parse_str($_POST['name'], $output);
+		$output = array_values($output);
+
+		//To use array names instead of index uncomment the following 2 lines under
 		// $params = array();
 		// parse_str($_POST, $params);
 
-		$output = array_values($output);
 
-		//This function will insert a new job category
-		//$output[0] = job category name
-		//$output[1] = job title description
-		insert_job_type($conn, $output[0],$output[1]);
+		if($output[2] === 'jobcategories') {
+			insert_job_type($conn, $output[0], $output[1]);
+		}
 
-		//This function will insert a new job title
+		//This function will insert a new job title 
+		if($output[3] === 'job') {
+			insert_job_title($conn, $output[0], $output[1], $output[2]);
+		}
 
-		//insert_job_title($conn, $output[0], $output[1]);
-
-		//$allKeys = array_keys($output);
-		//echo $allKeys[0];
-		//$output = array_values($output);
-
-		//touch('insertCTRL.txt');
-		// $file = fopen('test.txt', 'w');
-		// for($i = 0; $i < count($output); $i++){
-		// 	fwrite($file, $output[$i]);
-		// }
-		//fwrite($file, $companydescription);
-		//fclose($file);
-		//insert_job_type($output[0],$output[1]);
-		//insert_company("ryan","schurton");
 	}
 ?>
